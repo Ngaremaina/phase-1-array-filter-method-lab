db090987309a8d90b5af9fed2ac0b04335579201
Ngaremaina <ngaremaina4@gmail.com>
2023-03-21 12:41:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(
+        word => word === string || word === string.toLowerCase()
+    )
+}
+function fuzzyMatch(drivers, string){
+    return drivers.filter(
+        word => word[0] === string[0] || word[1]=== string[1]
+    )
+}
+
+function matchName(drivers, string){
+    return drivers.filter(
+        word => word.name === string 
+    )
+}
